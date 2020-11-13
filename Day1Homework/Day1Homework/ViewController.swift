595ca1d023844c55854705184862f4b289c536e8
MasaYanagida <kyoyah.rod@gmail.com>
2020-11-13 20:03:24 +0900

    Impl

--- Day1Homework/Day1Homework/ViewController.swift
+++ Day1Homework/Day1Homework/ViewController.swift
@@ -11,17 +11,72 @@ class ViewController: UIViewController {
     
     @IBOutlet fileprivate dynamic weak var tableView: UITableView! {
         didSet {
-            // TODO
+            tableView.delegate = self
+            tableView.dataSource = self
         }
     }
     var contents = [Content]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reload()
+        NotificationCenter.default.addObserver(
+          self, selector: #selector(onResume(_:)), name: UIApplication.didBecomeActiveNotification, object: nil
+        )
+        NotificationCenter.default.addObserver(
+          self, selector: #selector(onPause(_:)), name: UIApplication.didEnterBackgroundNotification, object: nil
+        )
     }
     deinit {
-        // TODO
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    
+    private func reload() {
+        contents = []
+        for _ in 0 ..< 10 {
+            contents.append(Content.create())
+        }
+        let offset = tableView.contentOffset
+        tableView.reloadData()
+        tableView.setContentOffset(offset, animated: false)
+    }
+    @objc private func onResume(_ notification: Foundation.Notification? = nil) {
+        guard isViewLoaded else { return }
+        reload()
+    }
+    @objc private func onPause(_ notification: Foundation.Notification? = nil) {
+        // do nothing
     }
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let content = contents[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = "\(content.name)\n\(content.address)"
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // do nothing
+    }
+}
