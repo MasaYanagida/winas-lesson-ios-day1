e2a8b093966370170d846f61b46766775964ffa7
watomo <solhinetran1611@yahoo.co.jp>
2020-11-24 19:37:23 +0900

    lession-ios-day1の課題を対応

--- Day1Homework/Day1Homework/ViewController.swift
+++ Day1Homework/Day1Homework/ViewController.swift
@@ -11,17 +11,46 @@ class ViewController: UIViewController {
     
     @IBOutlet fileprivate dynamic weak var tableView: UITableView! {
         didSet {
-            // TODO
+            startObserving()
         }
     }
     var contents = [Content]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
     deinit {
-        // TODO
+        NotificationCenter.default.removeObserver(self)
     }
+    
+    func startObserving() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.willEnterForeground),
+                                               name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+      @objc func willEnterForeground() {
+        contents = []
+        
+        for i in 1...10 {
+            contents.append(Content.create())
+        }
+        
+        tableView.reloadData()
+      }
+
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = contents[(indexPath as NSIndexPath).row].name
+        cell.detailTextLabel?.text = contents[(indexPath as NSIndexPath).row].address
+        return cell
+    }
+}
