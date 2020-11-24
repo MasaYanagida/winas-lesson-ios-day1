6f33c279b7fb0db02635cae7e64aec59715e38ef
Ahsan Ullah Rasel <r-ahsan@winas.jp>
2020-11-25 00:46:44 +0900

    Add cell for table view

---
+++ Day1Homework/Day1Homework/View/HomeTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewCell.swift
+//  Day1Homework
+//
+//  Created by Ahsan on 2020/11/24.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
