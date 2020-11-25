68165bc9b521e7c2c1bb6ebfdc4968cd21b5b8ad
Mahbubur Rahman Mishal <mishal.rahman996@gmail.com>
2020-11-25 13:14:21 +0900

    add table view cell with title and sub title and a nav bar

--- Day1Homework/Day1Homework/ViewController.swift
+++ Day1Homework/Day1Homework/ViewController.swift
@@ -8,20 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet fileprivate dynamic weak var tableView: UITableView! {
         didSet {
             // TODO
         }
     }
+    
     var contents = [Content]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        contents = getAddressData();
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
     deinit {
         // TODO
     }
+    
+    func getAddressData() -> [Content] {
+            
+        var addressList: [Content] = []
+        for _ in 0...10 {
+            let address = Content.create()
+            addressList.append(address)
+        }
+        return addressList
+    }
 }
 
+extension ViewController:UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped");
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell    = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let content = contents[indexPath.row]
+        cell.textLabel?.text = content.name;
+        cell.detailTextLabel?.text = content.address;
+        return cell
+    }
+}
